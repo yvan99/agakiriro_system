42e002e4fd94bb657cd8758be4ea374c4aeb2c49
yvan99 <ishimweyvan90@gmail.com>
2021-09-07 12:08:19 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require 'inc/css.php'; ?>
+
+<body>
+  <?php require 'inc/header.php' ?>
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+    <div class="container">
+      <h1>Ikaze ku gakiriro</h1>
+      <a href="#about" class="btn-get-started scrollto">Get Started</a>
+    </div>
+  </section><!-- End Hero -->
+  <?php require 'inc/footer.php' ?>
+  <?php require 'inc/js.php' ?>
---
+++ login.php
@@ -0,0 +1,7 @@
+﻿<?php require 'inc/css.php'; ?>
+
+<body>
+	<?php require 'inc/header.php' ?>
+	<h1>INNER PAGE</h1>
+	<?php require 'inc/footer.php' ?>
+	<?php require 'inc/js.php' ?>
